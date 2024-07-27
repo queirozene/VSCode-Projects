ab1b7fffb54b2a9a3fee6fbd50c951ba10328e56
Gustavo Queiroz Ribeiro <138177146+queirozene@users.noreply.github.com>
2024-07-27 13:01:39 -0300

    Update app.py

--- DSA - LAB2/app.py
+++ DSA - LAB2/app.py
@@ -2,5 +2,6 @@
 
 # Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos até aqui no curso.
 # A solução será apresentada no próximo capítulo!
+# alterado no github
 
 print("\n******************* Calculadora em Python *******************")
